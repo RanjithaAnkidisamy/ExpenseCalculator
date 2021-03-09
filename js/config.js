8744d9522bf58d22bf5d1a364294fd9fe167da83
Ranjitha <32982928+RanjithaAnkidisamy@users.noreply.github.com>
2021-03-09 22:06:09 +0530

    script added

---
+++ js/config.js
@@ -0,0 +1,11 @@
+window._config = {
+    cognito: {
+
+        userPoolId: 'ap-south-1_pr6MikXDH', // e.g. us-east-2_uXboG5pAb
+        region: 'ap-south-1', // e.g. us-east-2
+		clientId: '43ab3cullu35t14gmj31d1kinr' //is this used anywhere?
+
+    },
+    
+};
+
